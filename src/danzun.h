993a8195ee4981938de8416e8c60e8da30fc67d7
Orion Musselman <orionmusselman@gmail.com>
2020-11-14 16:30:35 -0900

    fixes

---
+++ src/danzun.h
@@ -0,0 +1,15 @@
+#ifndef DANZUN_DANZUN_GROUP_INCLUDE_H_INCLUDED_H
+#define DANZUN_DANZUN_GROUP_INCLUDE_H_INCLUDED_H
+
+#include "./ani.h"
+#include "./core.h"
+#include "./danzun.h"
+#include "./game.h"
+#include "./math.h"
+#include "./render.h"
+#include "./sprite.h"
+#include "./time.h"
+#include "./ui.h"
+#include "./win.h"
+
+#endif
--- src/win.h
+++ src/win.h
@@ -2,7 +2,7 @@
 #define DANZUN_WIN_GROUP_INCLUDE_H_INCLUDED_H
 
 #include "win/Cursor.h"
-#include "win/Window.h"
 #include "win/WindowEvent.h"
+#include "win/Window.h"
 
 #endif
