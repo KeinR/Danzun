dda2382f4e97f7dae6b35d14f0149c5c940ec570
Orion Musselman <orionmusselman@gmail.com>
2020-11-30 19:02:15 -0900

    Removed junk

--- src/ani.h
+++
@@ -1,8 +0,0 @@
-#ifndef DANZUN_ANI_GROUP_INCLUDE_H_INCLUDED_H
-#define DANZUN_ANI_GROUP_INCLUDE_H_INCLUDED_H
-
-#include "ani/AniControl.h"
-#include "ani/Animation.h"
-#include "ani/Frame.h"
-
-#endif
--- src/time.h
+++
@@ -1,9 +0,0 @@
-#ifndef DANZUN_TIME_GROUP_INCLUDE_H_INCLUDED_H
-#define DANZUN_TIME_GROUP_INCLUDE_H_INCLUDED_H
-
-#include "time/Clock.h"
-#include "time/Event.h"
-#include "time/RealTimer.h"
-#include "time/Timer.h"
-
-#endif
